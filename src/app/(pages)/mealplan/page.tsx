4846bc456b82658baee6ff427a8ee617cc8340fb
@allanparaiso29 <dev.reymark@gmail.com>
2025-04-14 18:42:51 +0800

    update page.tsx and providers.tsx

--- src/app/(pages)/mealplan/page.tsx
+++ src/app/(pages)/mealplan/page.tsx
@@ -10,6 +10,8 @@ import {
   ModalContent,
   ModalFooter,
   ModalHeader,
+  Select,
+  SelectItem,
   Table,
   TableBody,
   TableColumn,
@@ -17,33 +19,44 @@ import {
   useDisclosure,
 } from "@heroui/react";
 
+const days = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
 export default function MealPlan() {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
   return (
     <ProtectedRoute>
       <ApplicationLayout>
         <div className="p-6">
-          <h1 className="text-2xl font-bold">Weekly Meal Plan</h1>
-        </div>
+          <Table
+            className="mt-4"
+            aria-label="Items in your pantry"
+            topContent={
+              <div className="flex justify-between">
+                <h1 className="text-2xl font-bold">Weekly Meal Plan</h1>
 
-        <Table
-          className="mt-4"
-          aria-label="Items in your pantry"
-          topContent={
-            <div className="flex justify-end">
-              <Button color="primary" onPress={onOpen}>
-                Add Meals
-              </Button>
-            </div>
-          }
-        >
-          <TableHeader>
-            <TableColumn>Food</TableColumn>
-            <TableColumn>Day</TableColumn>
-            <TableColumn>Calorie</TableColumn>
-          </TableHeader>
-          <TableBody emptyContent={"No rows to display."}>{[]}</TableBody>
-        </Table>
+                <Button color="primary" onPress={onOpen}>
+                  Add Meals
+                </Button>
+              </div>
+            }
+          >
+            <TableHeader>
+              <TableColumn>Food</TableColumn>
+              <TableColumn>Day</TableColumn>
+              <TableColumn>Calorie</TableColumn>
+            </TableHeader>
+            <TableBody emptyContent={"No rows to display."}>{[]}</TableBody>
+          </Table>
+        </div>
 
         <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
           <ModalContent>
@@ -51,11 +64,12 @@ export default function MealPlan() {
               <>
                 <ModalHeader>Add Meals</ModalHeader>
                 <ModalBody>
-                  <Input
-                    label="Day"
-                    isRequired
-                    placeholder="Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday"
-                  />
+                  <Select isRequired label="Select">
+                    {days.map((day) => (
+                      <SelectItem key={day}>{day}</SelectItem>
+                    ))}
+                  </Select>
+
                   <Input label="Food" isRequired type="text" />
                 </ModalBody>
                 <ModalFooter>
